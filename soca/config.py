c217265c929bce985c2d5138899f6adb2613d8b4
Jasper Schulz <jasper.schulz@student.hpi.de>
2015-12-10 16:14:01 +0100

    typo

--- soca/config.py
+++ soca/config.py
@@ -9,7 +9,7 @@ password = ''
 import util
 import os
 
-_config = os.env.get('SOCA_CONFIG') or util.find_file('.socarc')
+_config = os.environ.get('SOCA_CONFIG') or util.find_file('.socarc')
 
 if _config:
     locals().update(util.read_config(_config))
